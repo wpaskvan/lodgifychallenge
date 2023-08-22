0e0c242d58efc8f8f3c5624ac84600c382573052
Walter Paskvan <walter.paskvan@cognizant.com>
2023-08-22 14:55:50 +0200

    Fix View Model to not expose sensitive data Extend DB for Login

--- SuperPanel.App/Models/UserViewModel.cs
+++ SuperPanel.App/Models/UserViewModel.cs
@@ -5,8 +5,6 @@ namespace SuperPanel.App.Models
     public class UserViewModel
     {
         public int Id { get; set; }
-
-        public string Login { get; set; }
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
